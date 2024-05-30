429ea62b7845d91f9e23266976c51170aedb1e1a
Кирилл <50743926+BillCipher890@users.noreply.github.com>
2024-05-30 21:15:33 +0400

    add docker compose

--- MessageExchangeTest/obj/Debug/net8.0/MessageExchangeTest.AssemblyInfo.cs
+++ MessageExchangeTest/obj/Debug/net8.0/MessageExchangeTest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MessageExchangeTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a51ed2a072ac520400f922b4d995d7bb43e108a9")]
 [assembly: System.Reflection.AssemblyProductAttribute("MessageExchangeTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MessageExchangeTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
